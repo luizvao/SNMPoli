0e09b759b53dcbd24da398cca083ccc79eb2b02a
Nestor Júnior <nestor.tmj@gmail.com>
2014-09-03 21:51:41 -0300

    Create a maven quickstart project

---
+++ src/main/java/br/poli/ecomp/gr/snmpoli/App.java
@@ -0,0 +1,13 @@
+package br.poli.ecomp.gr.snmpoli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
